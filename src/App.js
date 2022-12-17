0a193b8188b978fdb182e9a425a50a8756d416d2
Kim Eunjeong <ryomi.j78@gmail.com>
2022-12-17 22:17:04 +0900

    add fonts

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import Navigation from "./components/Navigation";
 import Home from "./routes/Home";
 import Introduction from "./routes/Introduction";
 import History from "./routes/History";
+import style from './App.css'
 
 function App() {
   return (
     <>
       <BrowserRouter>
-        <div style={{backgroundColor:"rgb(254, 247, 237)"}}>
+        <div className={style.App} style={{backgroundColor:"rgb(254, 247, 237)"}}>
         <Navigation />
         <Routes>
           <Route path={"/"} element={<Home />}></Route>
