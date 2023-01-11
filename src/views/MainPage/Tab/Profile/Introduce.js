bca752f87ac6a63172144fafab5b757d35f17db4
Kim Eunjeong <ryomi.j78@gmail.com>
2023-01-11 13:43:07 +0900

    add option to a tag to make it open in a new tab, resize github icon and edit resposive web style - font size and icon and page margin

--- src/views/MainPage/Tab/Profile/Introduce.js
+++ src/views/MainPage/Tab/Profile/Introduce.js
@@ -31,38 +31,92 @@ function Introduction() {
           <div className={styles.info}>
             <div className={styles.itemTitle}>Personal Info</div>
             <div className={styles.infoFrame}>
-              <p><FcBusinesswoman size="50"  className="icon"/></p>
-              <p>이름<br/>김은정</p>
-              <p><FcIphone size="50"  className="icon"/></p>
-              <p>연락처<br /> 010.4307.7246</p>
-              <p><GoMention size="40"  className="icon"/></p>
-              <p>이메일 <br/>ryomi.j78@gmail.com</p>
-              <p><GoMarkGithub size="40" className="icon"/></p>
-              <p><a className={styles.profileLink} href="https://github.com/Ryomi-j">Github</a></p>
-              <p><FcLink size="50" className="icon" /></p> 
-              <p><a className={styles.profileLink} href="https://premubo.tistory.com/">Blog</a></p>
+              <p>
+                <FcBusinesswoman size="50" className="icon" />
+              </p>
+              <p>
+                이름
+                <br />
+                김은정
+              </p>
+              <p>
+                <FcIphone size="50" className="icon" />
+              </p>
+              <p>
+                연락처
+                <br /> 010.4307.7246
+              </p>
+              <p>
+                <GoMention size="40" className="icon" />
+              </p>
+              <p>
+                이메일 <br />
+                ryomi.j78@gmail.com
+              </p>
+              <p >
+                <GoMarkGithub size="40" className="icon"/>
+              </p>
+              <p>
+                <a
+                  className={styles.profileLink}
+                  href="https://github.com/Ryomi-j"
+                >
+                  Github
+                </a>
+              </p>
+              <p>
+                <FcLink size="50" className="icon" />
+              </p>
+              <p>
+                <a
+                  className={styles.profileLink}
+                  href="https://premubo.tistory.com/"
+                  style={{marginLeft:"-90px"}}>
+                  Blog
+                </a>
+              </p>
             </div>
             <div className={styles.responsiveInfo}>
-              <p><FcBusinesswoman size="50" /><b>김은정</b></p>
-              <p><FcIphone size="50" />010.4307.7246</p>
-              <p><GoMention size="35" /> ryomi.j78@gmail.com</p>
-              <p><GoMarkGithub size="40" className="icon" /><a href="https://github.com/Ryomi-j"> Github</a></p>
-              <p><FcLink size="50" className="icon" /><a href="https://premubo.tistory.com/"> Blog</a></p>
+              <p style={{ fontFamily: "Dongle", fontSize: "30px" }}>
+                <FcBusinesswoman size="50" />
+                <b>김은정</b>
+              </p>
+              <p>
+                <FcIphone size="50" />
+                010.4307.7246
+              </p>
+              <p>
+                <GoMention size="35" /> ryomi.j78@gmail.com
+              </p>
+              <p>
+                <GoMarkGithub size="35" className="icon"  style={{marginRight:"10px"}}/>
+                <a href="https://github.com/Ryomi-j" target="_blank">
+                  Github
+                </a>
+              </p>
+              <p>
+                <FcLink size="50" className="icon"  style={{marginRight:"10px"}} />
+                <a href="https://premubo.tistory.com/" target="_blank">
+                  Blog
+                </a>
+              </p>
             </div>
           </div>
-          <div className={styles.itemTitle}>Skills</div>
-          <div className={styles.skillIcon}>
-            {skills.map((skill, index) => (
-              <div key={index}>
-                <img
-                  key={index}
-                  src={require("./assets/" + skill.img + ".png")}
-                  alt={skill.title}
-                  className={styles.logos}
-                />
-              </div>
-            ))}
-          </div>
+          <section className={styles.skillwrap}>
+            <div className={styles.itemTitle}>Skills</div>
+            <div className={styles.skillIcon}>
+              {skills.map((skill, index) => (
+                <div key={index}>
+                  <img
+                    key={index}
+                    src={require("./assets/" + skill.img + ".png")}
+                    alt={skill.title}
+                    className={styles.logos}
+                  />
+                </div>
+              ))}
+            </div>
+          </section>
         </div>
       </div>
     </div>
